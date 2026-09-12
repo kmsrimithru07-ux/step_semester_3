89e1e46eb62ddd7140486096320f3692644e46a5
kmsrimithru07-ux <kmsrimithru07@gmail.com>
2026-09-12 16:29:22 +0530

    Update week 1 assignments and class problems

---
+++ src/main/java/week_1/assignment_problem/WordLengthProfiler.java
@@ -0,0 +1,32 @@
+package main.java.week_1.assignment_problem;
+public class WordLengthProfiler {
+
+    static void classifyWordLengths(String review) {
+        String[] words = review.split(" ");
+
+        int shortWords = 0;
+        int mediumWords = 0;
+        int longWords = 0;
+
+        for (String word : words) {
+            int length = word.length();
+
+            if (length >= 1 && length <= 4) {
+                shortWords++;
+            } else if (length <= 8) {
+                mediumWords++;
+            } else {
+                longWords++;
+            }
+        }
+
+        System.out.println("Short: " + shortWords);
+        System.out.println("Medium: " + mediumWords);
+        System.out.println("Long: " + longWords);
+    }
+
+    public static void main(String[] args) {
+        String review = "This movie was absolutely fantastic and thrilling";
+        classifyWordLengths(review);
+    }
+}
